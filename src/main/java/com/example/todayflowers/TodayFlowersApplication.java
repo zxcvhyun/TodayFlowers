cd933e9742fdf3ea2ba052a1685670cd51ee6b68
zxcvhyun <76957511+zxcvhyun@users.noreply.github.com>
2021-09-15 21:08:35 +0900

    Initial commit

---
+++ src/main/java/com/example/todayflowers/TodayFlowersApplication.java
@@ -0,0 +1,13 @@
+package com.example.todayflowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayFlowersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayFlowersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todayflowers/TodayFlowersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todayflowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayFlowersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
