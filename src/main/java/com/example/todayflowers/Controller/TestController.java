08d78dade27cbe4c812758e708cb204b7dd583a0
zxcvhyun <76957511+zxcvhyun@users.noreply.github.com>
2021-09-15 23:38:56 +0900

    Test요청파일

---
+++ src/main/java/com/example/todayflowers/Controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.todayflowers.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/test/get")
+    public String getString(){
+        return "Get요청";
+    }
+
+    @GetMapping("/test/path-variable/{value}")
+    public String getValue(@PathVariable String value){
+        return value;
+    }
+
+}
