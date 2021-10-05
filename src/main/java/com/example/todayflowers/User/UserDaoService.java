c6f1d9eec46a7cb15f1b27bf664250628bcd9e30
zxcvhyun <76957511+zxcvhyun@users.noreply.github.com>
2021-10-05 18:14:10 +0900

    Post요청 추가

---
+++ src/main/java/com/example/todayflowers/User/UserDaoService.java
@@ -0,0 +1,27 @@
+package com.example.todayflowers.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class UserDaoService {
+//    @Autowired
+//    private UserRepository userRepository;
+//    private User users = new User();
+//
+//    public User updateById(int id, String useremail) {
+//        Optional<User> user = userRepository.findById(id);
+//        if (!user.isPresent()) {
+//            return null;
+//        }else{
+//            users.setUseremail(useremail);
+//
+//        }
+//
+//
+//
+//    }
+}
