7c37497429d1ccce1744ca104772a7701602e9c9
zxcvhyun <76957511+zxcvhyun@users.noreply.github.com>
2021-10-05 20:15:47 +0900

    컨트롤러

--- src/main/java/com/example/todayflowers/TodayFlowersApplication.java
+++ src/main/java/com/example/todayflowers/TodayFlowersApplication.java
@@ -1,5 +1,6 @@
 package com.example.todayflowers;
 
+import com.example.todayflowers.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = UserController.class)
 public class TodayFlowersApplication {
 
     public static void main(String[] args) {
