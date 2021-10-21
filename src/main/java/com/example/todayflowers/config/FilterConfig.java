5e57a462fea47b168c77a327aa555d8515af6695
zxcvhyun <76957511+zxcvhyun@users.noreply.github.com>
2021-10-21 21:29:56 +0900

    springSecurity JWT 토큰

--- src/main/java/com/example/todayflowers/config/FilterConfig.java
+++ src/main/java/com/example/todayflowers/config/FilterConfig.java
@@ -1,18 +1,18 @@
-//package com.example.todayflowers.config;
-//
-//import com.example.todayflowers.filter.MyFilter1;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class FilterConfig {
-//    @Bean
-//    public FilterRegistrationBean<MyFilter1> filter1() {
-//        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
-//        bean.addUrlPatterns("/*");
-//        bean.setOrder(0); // 낮은 번호가 필터중에서 가장 먼저 실행
-//        return bean;
-//
-//    }
-//}
+package com.example.todayflowers.config;
+
+import com.example.todayflowers.filter.MyFilter1;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<MyFilter1> filter1() {
+        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+        bean.addUrlPatterns("/*");
+        bean.setOrder(0); // 낮은 번호가 필터중에서 가장 먼저 실행
+        return bean;
+
+    }
+}
